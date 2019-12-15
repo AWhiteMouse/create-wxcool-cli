695fd5ec8d24bdc99b57b905b6b5e36c6398acb8
chenxin <1586671591@qq.com>
2019-12-15 15:59:54 +0800

    perf: 优化代码

--- bin/index.js
+++ bin/index.js
@@ -10,88 +10,13 @@ const program = require('commander')
 const symbols = require('log-symbols')
 
 program
+    .option('-i --init', 'init object')
     .version(require('../package').version, '-v, --version')
     .command('init <name>')
     .alias('i')
     .description('install mini program——WxCool')
     .action((name) => {
-        if (!fs.existsSync(name)) {
-            inquirer.prompt([
-                {
-                    name: 'description',
-                    message: 'please enter program description:',
-                },
-                {
-                    name: 'author',
-                    message: 'please enter program author:',
-                },
-                {
-                    name: 'appid',
-                    message: 'please enter program appid:'
-                }
-            ]).then((answers) => {
-                console.log(answers)
-                const spinner = ora('Create Program...')
-                spinner.start()
-                const downloadPath = 'AWhiteMouse/WxCool'
-                download(
-                    downloadPath,
-                    name,
-                    { clone: true },
-                    (err) => {
-                        if (err) {
-                            spinner.fail()
-                            console.error(
-                                symbols.error,
-                                chalk.red(`${err}create project fail,please check your network connection and try again`)
-                            )
-                            process.exit(1)
-                        }
-                        const packageJson = {
-                            name,
-                            description: answers.description,
-                            author: answers.author,
-                        }
-                        const packageFile = `${name}/package.json`
-                        const packageContent = fs.readFileSync(packageFile).toString()
-                        const packageResult = handlebars.compile(packageContent)(packageJson)
-                        fs.writeFileSync(packageFile, packageResult)
-                        const projectJson = {
-                            projectname: name,
-                            description: answers.description,
-                            appid: answers.appid,
-                        }
-                        const projectFile = `${name}/src/project.config.json`
-                        const projectContent = fs.readFileSync(projectFile).toString()
-                        const projectResult = handlebars.compile(projectContent)(projectJson)
-                        fs.writeFileSync(projectFile, projectResult)
-                        const appJson = {
-                            "window.navigationBarTitleText": name,
-                        }
-                        const appFile = `${name}/src/app.json`
-                        const appContent = fs.readFileSync(appFile).toString()
-                        const appResult = handlebars.compile(appContent)(appJson)
-                        fs.writeFileSync(appFile, appResult)
-                        spinner.succeed()
-                        console.log(
-                            symbols.success,
-                            chalk.green('Success!')
-                        )
-                        console.log()
-                        console.log('========================')
-                        console.log()
-                        console.log(`      $ cd ${name}      `)
-                        console.log('      $ npm i           ')
-                        console.log('      $ npm run dev     ')
-                        console.log()
-                        console.log('========================')
-                        console.log()
-                    })
-            })
-        } else {
-            // 错误提示项目已存在，避免覆盖原有项目
-            console.error(symbols.error, chalk.red('Project Had Exist!'))
-        }
+        require("../lib/init")(name)
     }).on('--help', () => {
         console.log()
         console.log('Examples:')
